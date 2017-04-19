9d407c8e2f00a8ee265f2103d850a1c324d3749d
Federico Fissore <federico@fissore.org>
2017-04-19 17:09:22 +0200

    more cleanup when synchronizing

--- sync.bash
+++ sync.bash
@@ -5,4 +5,9 @@ IFS=$'\n\t'
 
 mvn clean compile archetype:create-from-project
 
-rsync -av --delete --progress --exclude=.git target/generated-sources/archetype/ ../java8-jersey2-guice4-webapp-archetype/
+rsync -av --delete --progress --exclude=.git --exclude=.gitignore --exclude=README.md --exclude=LICENSE.txt --exclude=.idea target/generated-sources/archetype/ ../java8-jersey2-guice4-webapp-archetype/
+
+rm -rf ../java8-jersey2-guice4-webapp-archetype/target
+rm -rf ../java8-jersey2-guice4-webapp-archetype/src/test
+
+find ../java8-jersey2-guice4-webapp-archetype/ -type f -exec dos2unix {} \;
